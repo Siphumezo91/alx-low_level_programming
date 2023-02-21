9f784ea65d01e4435f5e37daefec6331434680bb
mozo <mozo@gmail.com>
2023-02-21 12:46:50 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet - prints the alpha in lowercase
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -11,4 +12,5 @@ void print_alphabet(void)
 		_putchar(la)
 
 			_putchar('\n');
+	return (0);
 }
