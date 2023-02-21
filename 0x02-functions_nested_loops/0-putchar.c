88b7cc443da59b70ee2c5063cd1ec552ad142065
mozo <mozo@gmail.com>
2023-02-21 10:53:40 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * written by
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchar(',');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
