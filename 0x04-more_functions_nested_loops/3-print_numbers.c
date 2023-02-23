fce0fedee16a9a955df57219a68974123f43bc74
mozo <mozo@gmail.com>
2023-02-23 12:50:13 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_number - Print the numbers 0 up to 9
- * Return: The numbers since 0 up to 9
+ * Return: void
  */
 
 void print_numbers(void)
 {
-	int x;
+	char c;
 
-	for  (x = 0; x <= 9; x++)
+	for  (c = 0; c <= 9; c++)
 	{
-		_putchar(x + '0');
+		_putchar(c + '0');
 	}
 	_putchar('\n');
 }
