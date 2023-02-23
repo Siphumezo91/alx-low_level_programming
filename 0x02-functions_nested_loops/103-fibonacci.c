ef004008e75a63c38183a67ce1a538d56afa26ef
mozo <mozo@gmail.com>
2023-02-22 16:33:55 -0800

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poinit
+ * values do not exceed 4,000,000
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
+		++i;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
