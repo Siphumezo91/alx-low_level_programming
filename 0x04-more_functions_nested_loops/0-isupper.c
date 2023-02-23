8ad63d889cfe9e3964d4b87e1a79c85c9148b16b
mozo <mozo@gmail.com>
2023-02-23 10:57:57 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if the letter is upper
+ * Return: 1 if c is uppercase, otherwise 0
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
