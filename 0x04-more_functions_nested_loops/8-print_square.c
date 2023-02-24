17cecf8a375da2fab45d0f2a6d02ae5461c3063a
mozo <mozo@gmail.com>
2023-02-23 18:00:04 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,21 +8,21 @@
 
 void print_square(int size)
 {
+	int x, y;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int i, j;
-
-		for (i = 0; i < size; i++)
+		for (x = 0; x < size; x++)
 		{
-			for (j = 0; j < zise; j++)
+			for (y = 0; y < size; y++)
 			{
-				_putchar('#');
+				_putchar(35);
 			}
-			_putchar(''\n');
+			_putchar('\n');
 		}
 	}
 }
