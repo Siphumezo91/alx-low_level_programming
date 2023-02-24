85ad9f2b45f3bdfd0173a3004c83698325aa0c46
mozo <mozo@gmail.com>
2023-02-23 17:40:07 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,22 +8,24 @@
 
 void print_diagonal(int n)
 {
-	int x, y;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (x = 0; x < n; x++)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (y = 0; y < x; y++)
+			for (j = 0; j < n; j++)
 			{
-				_putchar(12);
-			}
-			_putchar(92);
-			_putchar('\n');
+				if (j == i)
+				_putchar('\\');
+			else if (j < i)
+				_putchar(' ');
 		}
+		_putchar('\n');
+	}
 	}
 }
