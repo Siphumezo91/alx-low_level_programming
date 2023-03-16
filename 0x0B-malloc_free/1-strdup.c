3d000e343a0d5f8cdb18b5969401597833de299d
mozo <mozo@gmail.com>
2023-03-16 01:36:34 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,14 +17,14 @@ char *_strdup(char *str)
 		return (NULL);
 
 	i = 0;
-	while (str[i] != '\0\)
-			i++
-			aaa = malloc(sizeof(char) * (i + 1));
+	while (str[i] != '\0')
+		i++;
+	aaa = malloc(sizeof(char) * (i + 1));
 
-			if (aaa == NULL)
-			return (NULL);
+	if (aaa == NULL)
+		return (NULL);
 
-			for (r = 0; str[r]; r++)
-			aaa[r] = str[r];
-			return (aaa);
-			}
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
+	return (aaa);
+}
